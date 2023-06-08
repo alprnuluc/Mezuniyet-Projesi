266b71b3686c83aab3337fdaf7171a992d74c74f
alprnuluc <128415069+alprnuluc@users.noreply.github.com>
2023-06-08 19:39:46 +0300

    Add files via upload

---
+++ hastaneotomasyon8/header.php
@@ -0,0 +1,36 @@
+<?php 
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Hastane Randevu Otomasyon Sistemi</title>
+</head>
+<body>
+    <div class="ustbar">
+        <a href="anasayfa.php"><h1>Hastane Randevu Otomasyon</h1></a>
+
+        <div class="menu">
+        <a href="hesap.php"><h5>Hesap Bilgileriniz</h5></a>
+        <a href="randevu.php"><h5>Randevu Bilgileriniz</h5></a>
+        </div>
+    </div>
+    
+
+    <div class="cikis">Çıkış Yap</div>
+
+
+
+    
+</body>
+</html>
---
+++ hastaneotomasyon8/hesap.php
@@ -0,0 +1,46 @@
+<?php 
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hastane Randevu Otomasyon Sistemi</title>
+</head>
+<body>
+    <div class="hesapcontent">
+        <div class="label">
+            <label>ADI</label>
+            <input type="text">
+
+        </div> <br>
+        <div class="label">
+            <label>SOYADI</label>
+            <input type="text">
+
+        </div> <br>
+        <div class="label">
+            <label>TC Kimlik</label>
+            <input type="text">
+
+        </div> <br>
+        <div class="label">
+            <label>TELEFON</label>
+            <input type="text">
+
+        </div> <br>
+        <div class="label">
+            <label>MAIL</label>
+            <input type="text">
+
+        </div> <br>
+        <button type="submit">Güncelle</button>
+    </div>
+
+
+    
+</body>
+</html>
